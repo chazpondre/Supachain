9b92c7bda5566e61d2309f8f09560cac4154ddda
Che Andre <che@thenightcrew.ca>
2024-08-18 14:24:08 -0400

    feat: Introduce CommonRequest sealed interface and subtypes for handling AI requests (part of #19)
    Several subtypes (CommonChatRequest, CommonEmbedRequest, CommonModRequest, CommonAudioRequest, CommonImageRequest) to provide a structured way to represent various types of requests that can be made to Providers.

---
+++ src/main/kotlin/robot/messenger/messaging/Request.kt
@@ -0,0 +1,16 @@
+/*
+░░░░░░░░░░░░░░░░░░░░░░░░░░       ░░░        ░░░      ░░░  ░░░░  ░░        ░░░      ░░░        ░░░░░░░░░░░░░░░░░░░░░░░░░░
+▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       ▓▓▓      ▓▓▓▓  ▓▓ ▓  ▓▓  ▓▓▓▓  ▓▓      ▓▓▓▓▓      ▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+██████████████████████████  ███  ███  ████████  ███   ██  ████  ██  ██████████████  █████  █████████████████████████████
+██████████████████████████  ████  ██        ███      █ ██      ███        ███      ██████  █████████████████████████████
+ */
+package dev.supachain.robot.messenger.messaging
+
+sealed interface CommonRequest
+
+internal interface CommonChatRequest : CommonRequest
+internal interface CommonEmbedRequest : CommonRequest
+internal interface CommonModRequest : CommonRequest
+internal interface CommonAudioRequest : CommonRequest
+internal interface CommonImageRequest : CommonRequest
