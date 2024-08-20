2f7af73e44a68e1e2d07210095c615fb1348e211
Che Andre <che@thenightcrew.ca>
2024-08-20 19:44:44 -0400

    Introduce ToolUseStrategy interface
    Adds the `ToolUseStrategy` interface to define strategies for tool usage. This interface provides a foundation for different approaches to deciding when and how to utilize external tools or functions to augment AI responses and capabilities.

---
+++ src/main/kotlin/robot/tool/stategies/ToolUseStrategy.kt
@@ -0,0 +1,21 @@
+package dev.supachain.robot.tool.stategies
+
+import dev.supachain.robot.director.Director
+import dev.supachain.robot.messenger.messaging.Message
+import dev.supachain.robot.tool.ToolConfig
+import dev.supachain.robot.tool.ToolMap
+
+/**
+ * Represents a strategy for using tools in AI interactions.
+ *
+ * This sealed interface defines the contract for different approaches to utilizing tools or functions
+ * within the AI system. Concrete implementations of this interface will dictate how the AI decides
+ * when and how to call external tools or functions to enhance its responses or capabilities.
+ *
+ * @since 0.1.0-alpha
+ * @author Che Andre
+ */
+sealed interface ToolUseStrategy {
+    fun message(director: Director<*, *, *>): Message?
+    fun getTools(toolMap: ToolMap): List<ToolConfig>
+}
