3d493ca52843e947e2803d75e34081959f72a16d
Che Andre <che@thenightcrew.ca>
2024-08-20 19:14:05 -0400

    Refactor Provider: Introduce feature map and Actions interface
    This commit refactors the `Provider` class to enhance functionality and modularity:
    
    Feature map: Introduces a `featureMap` to map `Feature` enums to corresponding `ProviderFeatureRequest` actions. This allows for dynamic feature selection and execution..
    Improved code structure:  Uses LWR fluent configuration using the `invoke` operator

--- src/main/kotlin/robot/provider/Provider.kt
+++ src/main/kotlin/robot/provider/Provider.kt
@@ -1,5 +1,8 @@
 package dev.supachain.robot.provider
 
+import dev.supachain.robot.director.DirectorCore
+import dev.supachain.robot.messenger.messaging.CommonResponse
+
 /**
  * Abstract base class for model providers.
  *
@@ -12,4 +15,31 @@ package dev.supachain.robot.provider
  * @since 0.1.0-alpha
  * @author Che Andre
  */
-abstract class Provider<T : Provider<T>>: Actions
+abstract class Provider<T : Provider<T>>: Actions{
+    internal abstract var maxRetries: Int
+    internal abstract var toolsAllowed: Boolean
+    internal var loopDetection: Boolean = true
+    private val featureMap: Map<Feature, ProviderFeatureRequest> = getFeatureMap()
+
+    /**
+     * Executes a request to the AI provider for a specific feature.
+     *
+     * This function dynamically dispatches the request to the appropriate handler
+     * based on the `featureMap`.
+     *
+     * @param feature The AI feature to be executed (e.g., `Feature.CHAT`, `Feature.EMBEDDING`).
+     * @param director The `Director` instance responsible for orchestrating the AI interaction.
+     * @return A `CommonResponse` object containing the result of the feature execution.
+     * @throws IllegalStateException If the requested feature is not supported by this AI provider.
+     *
+     * @since 0.1.0-alpha
+     * @author Che Andre
+     */
+    internal suspend inline
+    fun request(feature: Feature, director: DirectorCore): CommonResponse = this.featureMap[feature]?.invoke(director)
+        ?: throw IllegalStateException("Unsupported Feature: The $feature feature is not support by $name")
+
+    @Suppress("UNCHECKED_CAST")
+    internal inline operator
+    fun invoke(modify: T.() -> Unit) = (this as T).modify()
+}
