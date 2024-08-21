31836e56bd2f334c9c68d00da7c52acfc60ddcff
Che Andre <che@thenightcrew.ca>
2024-08-21 00:26:30 -0400

    Introduced Default Directives for Common Interactions
    - `NoTools`: Represents a directive that indicates the absence of any tools.
    -`Chat`: Defines a directive for general chat interactions, providing a prompt and expecting a string response.
    -`ChatMarkdown`: Defines a directive for chat interactions requiring a Markdown-formatted response.
    
    These default directives offer a convenient starting point for Robot interactions, providing standardized prompts and expected output formats. They can be used directly or extended to create more specific directives tailored to particular use cases.

---
+++ src/main/kotlin/robot/Defaults.kt
@@ -0,0 +1,20 @@
+package dev.supachain.robot
+
+import dev.supachain.robot.director.directive.FromSystem
+import dev.supachain.robot.answer.Answer
+
+interface Defaults {
+    interface NoTools
+
+    interface Chat {
+        @FromSystem("You are an helpful assistant. If you guess the answer, you should always use the tool if available." +
+                " If you are uncertain about the answer, state that.\n")
+        fun chat(prompt: String): Answer<String>
+    }
+
+    interface ChatMarkdown {
+        @FromSystem("You are an helpful assistant. If you guess the answer, you should always use the tool if available." +
+                " Your answer must be in Markdown formating. If you are uncertain about the answer, state that.\n")
+        fun chat(prompt: String): Answer<String>
+    }
+}
