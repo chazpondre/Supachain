e1290cfd2a768041ce3790f57b84b6d6e5359e84
Che Andre <che@thenightcrew.ca>
2024-08-20 20:02:02 -0400

    Refactor Director: Tool registration and API proxy
    This commit expands the functionalities of the `Director` class:
    
    Tool registration: Introduces the `setToolset` function. This function utilizes reflection to automatically register tools based on methods annotated with `@Tool` and `@Parameters`or clases annotated with @ToolSet within a provided `ToolInterface`.
    
    API proxy: Implements the `setUpDirectives` function. This function dynamically creates a proxy for a specified `API` interface. The proxy intercepts method calls and utilizes the available toolset and configurations.

--- src/main/kotlin/robot/director/Director.kt
+++ src/main/kotlin/robot/director/Director.kt
@@ -8,6 +8,7 @@
 
 package dev.supachain.robot.director
 
+import dev.supachain.robot.answer.Answer
 import dev.supachain.robot.messenger.Messenger
 import dev.supachain.robot.director.directive.Directive
 import dev.supachain.robot.provider.Provider
@@ -16,7 +17,11 @@ import dev.supachain.utilities.*
 import kotlinx.coroutines.CompletableJob
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import kotlin.coroutines.CoroutineContext
+import kotlin.reflect.KVisibility
 
 internal interface DirectorCore {
     val defaultProvider: Provider<*>
@@ -67,4 +72,64 @@ data class Director<P : Provider<*>, API : Any, ToolType : Any>(
     override val job: CompletableJob = SupervisorJob(),
     override val coroutineContext: CoroutineContext = Dispatchers.IO + job,
     override val messenger: Messenger = Messenger(),
-) : DirectorCore, RunsInBackground
+) : DirectorCore, RunsInBackground, Extensions{
+
+    lateinit var toolProxyObject: () -> ToolType
+
+    @Suppress("unused")
+    private val logger: Logger by lazy { LoggerFactory.getLogger(Director::class.java) }
+
+    /**
+     * Registers a tool interface by extracting its configuration from each method and corresponding annotations.
+     *
+     * This function utilizes reflection to identify methods annotated with @[Tool] and @[Parameters],
+     * subsequently creating [RobotTool] objects from these methods and adding them to the toolset.
+     * The function also supports the inclusion of methods from superinterfaces.
+     *
+     * @param ToolInterface The type of the tool to be registered. This should be an interface containing
+     *                 methods annotated with `@Tool` and `@Parameters`.
+     * @return An instance of `ExtendedClient<CLIENT, API>`, allowing for method chaining.
+     *
+     * @since 0.1.0-alpha
+     * @author Che Andre
+     */
+    internal inline fun <reified ToolInterface : ToolType> setToolset(): Director<P, API, *> = this.also {
+        if (defaultProvider.toolsAllowed) {
+            toolProxyObject = {
+                if(ToolInterface::class.visibility != KVisibility.PUBLIC)
+                    throw IllegalArgumentException("Your class must not have private visibility")
+                createObjectFromNoArgClass<ToolInterface>()
+            }
+
+            val tools = ToolInterface::class.getToolMethods().map { it.toToolConfig() }
+            logger.debug(
+                Debug("Director"),
+                "[Configuration]\nUses Toolset: {},\nTools: {}",
+                ToolInterface::class.simpleName,
+                tools
+            )
+            tools.forEach { toolMap[it.function.name] = it }
+        }
+    }
+
+    /**
+     * Creates a proxy instance of the specified API interface.
+     *
+     * This function maps the methods of the API interface to their corresponding [Directive]
+     * objects and creates a proxy that intercepts method calls and handles them using the provided
+     * toolset and configurations.
+     *
+     * @param API The type of the API interface.
+     * @return A proxy instance implementing the specified API interface.
+     *
+     * @since 0.1.0-alpha
+     * @author Che Andre
+     * */
+    internal inline fun <reified API : Any> setUpDirectives(): API {
+        val directiveInterface = API::class
+        directives.putAll(directiveInterface.getDirectives().associateBy { it.name })
+        return directiveInterface by { parent, name, args, returnType ->
+            Answer<Any>(returnType, scope.async { TODO() })
+        }
+    }
+}
