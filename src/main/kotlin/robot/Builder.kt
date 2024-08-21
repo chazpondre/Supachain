14a1300203eac178295f1853f793b4ca3fbaf8c9
Che Andre <che@thenightcrew.ca>
2024-08-20 23:33:27 -0400

    Introduced Robot Builder for Simplified Configuration
    This commit introduces a new entry point for Robot interaction through the `Robot` object. The `Robot` object provides a builder-style function `invoke` for creating and configuring a `Director` instance. Key benefits include:
    
    - Simplified Configuration: The builder approach simplifies the process of setting up a `Director` by handling provider instantiation, director creation, and configuration steps.
    - Type Safety: Leverages reified generics for `RobotProvider`, `Interface`, and `Tools` to ensure type safety and avoid runtime errors.

---
+++ src/main/kotlin/robot/Builder.kt
@@ -0,0 +1,41 @@
+package dev.supachain.robot
+
+import dev.supachain.CompileTime
+import dev.supachain.robot.provider.Provider
+import dev.supachain.robot.director.Director
+
+object Robot {
+    /**
+     * Creates and configures a `Director` instance for interacting with a model.
+     *
+     * This inline operator function streamlines the process of setting up a `Director` object by:
+     * 1. **Creating the Provider:** Instantiates a `RobotProvider` using its default constructor, handling potential errors.
+     * 2. **Initializing the Director:** Creates a `Director` instance with the specified `RobotProvider`, `Interface`, and `Tools`.
+     * 3. **Applying Modifications:** Applies the `modify` lambda function to the `Director`, allowing for custom configuration.
+     * 4. **Setting up Tools and Directives:** Calls `setToolset` and `setUpDirectives` to register the provided tools and map the methods of the `Interface` to corresponding directives.
+     *
+     * @param Interface The interface defining the available interactions with the [Robot].
+     * @param Tools The class containing the tools that the [Robot] can utilize.
+     * @param modify A lambda function to customize the configuration of the `Director`.
+     * @return An instance of the specified `Interface` type, acting as a proxy for interacting with the [Robot].
+     *
+     * @since 0.1.0-alpha
+     * @author Che Andre
+     */
+    @JvmStatic
+    @CompileTime
+    internal inline operator
+    fun <reified RobotProvider : Provider<RobotProvider>, reified Interface : Any, reified Tools : Any>
+            invoke(modify: Director<RobotProvider, Interface, Tools>.() -> Unit = {}): Interface {
+        val provider = try {
+            RobotProvider::class.java.getDeclaredConstructor().newInstance()
+        } catch (e: Exception) {
+            throw RuntimeException("Error creating instance of ${RobotProvider::class.simpleName}: ${e.message}", e)
+        }
+
+        return Director<RobotProvider, Interface, Tools>(provider) //{ createObjectFromInterfaceType<Tools>() }
+            .apply(modify)
+            .setToolset<Tools>()
+            .setUpDirectives<Interface>()
+    }
+}
