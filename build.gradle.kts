d8c3ff5313ee983a876e1ab2a54b9716b39a399c
chazpondre <che@thenightcrew.ca>
2024-10-02 18:19:05 -0400

    Added maven publishing to gradle and publish-to-github workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,22 @@ repositories {
 
 publishing {
     publications {
-        create<MavenPublication>("mavenJava") {
+        create<MavenPublication>("gpr") {
             from(components["java"]) // Publish the Java component
             groupId = project.group.toString() // Group ID for Maven
             artifactId = project.name // Artifact ID (name of the project)
             version = project.version.toString() // Project version
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/chazpondre/supachain")
+            credentials {
+                username = project.findProperty("GITHUB_ACTOR") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("GITHUB_TOKEN") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
         }
     }
 }
